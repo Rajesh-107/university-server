563c7b985ee5f22e18030a9183b6c00cba7b982b
Rajesh-107 <applerb326@gmail.com>
2024-01-20 21:21:57 +0600

    initial config

--- dist/app/server.js
+++ dist/app/server.js
@@ -1,4 +1,5 @@
 "use strict";
+// import { Server } from "http"
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,18 +9,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-const PORT = 5000;
-let server;
-function bootstrap() {
+// import app from "./app"
+// const PORT = 5000
+// let server:Server;
+// async function bootstrap(){
+//     server =  app.listen(PORT, () => {
+//         console.log(`Example app listening on port ${PORT}`)
+//       })
+// }
+// bootstrap();
+const mongoose = require('mongoose');
+function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        server = app_1.default.listen(PORT, () => {
-            console.log(`Example app listening on port ${PORT}`);
-        });
+        yield mongoose.connect(process.env.DATABASE_URL);
     });
 }
-bootstrap();
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,13 +1,21 @@
-import { Server } from "http"
-import app from "./app"
-const PORT = 5000
+// import { Server } from "http"
 
-let server:Server;
+// import app from "./app"
+// const PORT = 5000
 
-async function bootstrap(){
-    server =  app.listen(PORT, () => {
-        console.log(`Example app listening on port ${PORT}`)
-      })
-}
+// let server:Server;
 
-bootstrap();
+// async function bootstrap(){
+//     server =  app.listen(PORT, () => {
+//         console.log(`Example app listening on port ${PORT}`)
+//       })
+// }
+
+// bootstrap();
+const mongoose = require('mongoose');
+
+async function main() {
+    await mongoose.connect(process.env.DATABASE_URL);
+  
+    
+  }
